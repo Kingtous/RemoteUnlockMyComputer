bc29ab54e3164910deb465e4841364a6b387fa03
Kingtous <kingtous@qq.com>
2019-03-08 12:57:57 +0800

    3.8_修复指纹一直在检测的bug

--- src/main/java/com/kingtous/remotefingerunlock/ToolFragment/Unlock.java
+++ src/main/java/com/kingtous/remotefingerunlock/ToolFragment/Unlock.java
@@ -2,6 +2,7 @@ package com.kingtous.remotefingerunlock.ToolFragment;
 
 import android.app.Activity;
 import android.app.KeyguardManager;
+import android.content.Context;
 import android.hardware.fingerprint.FingerprintManager;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
@@ -19,71 +20,66 @@ import com.kingtous.remotefingerunlock.R;
 import java.util.Objects;
 
 
-public class Unlock extends Fragment{
+public class Unlock extends Fragment {
 
     FingerprintManager fingerprintManager;
     KeyguardManager keyguardManager;
     CancellationSignal cancellationSignal;
     FingerprintManager.AuthenticationCallback authenticationCallback;
 
-
-    public Unlock(){
+    Context context;
+    public Unlock() {
 
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.unlock,container,false);
+        return inflater.inflate(R.layout.unlock, container, false);
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        fingerprintManager=(FingerprintManager) Objects.requireNonNull(getActivity()).getSystemService(Activity.FINGERPRINT_SERVICE);
-        keyguardManager=(KeyguardManager)getActivity().getSystemService(Activity.KEYGUARD_SERVICE);
-        cancellationSignal=new CancellationSignal();
+        fingerprintManager = (FingerprintManager) Objects.requireNonNull(getActivity()).getSystemService(Activity.FINGERPRINT_SERVICE);
+        keyguardManager = (KeyguardManager) getActivity().getSystemService(Activity.KEYGUARD_SERVICE);
+        cancellationSignal = new CancellationSignal();
+        context=getContext();
         //检测是否有硬件
-        if (!fingerprintManager.isHardwareDetected()){
-            Toast.makeText(getContext(),"没检测到相关指纹硬件，指纹解锁可能不生效",Toast.LENGTH_LONG).show();
+        if (!fingerprintManager.isHardwareDetected()) {
+            Toast.makeText(getContext(), "没检测到相关指纹硬件，指纹解锁可能不生效", Toast.LENGTH_LONG).show();
         }
         initCallBack();
         startFingerListening();
 
-
-
-
     }
 
-    private void initCallBack(){
-        authenticationCallback=new FingerprintManager.AuthenticationCallback() {
+    private void initCallBack() {
+        authenticationCallback = new FingerprintManager.AuthenticationCallback() {
             @Override
             public void onAuthenticationError(int errorCode, CharSequence errString) {
-
-                Toast.makeText(getContext(),errString, Toast.LENGTH_LONG).show();
+                //Toast.makeText(context, errString, Toast.LENGTH_LONG).show();
             }
 
             @Override
             public void onAuthenticationHelp(int helpCode, CharSequence helpString) {
-
-                Toast.makeText(getContext(),helpString, Toast.LENGTH_LONG).show();
+                Toast.makeText(context, helpString, Toast.LENGTH_LONG).show();
             }
 
             @Override
             public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
-                Toast.makeText(getContext(),"指纹认证成功", Toast.LENGTH_LONG).show();
+                Toast.makeText(context, "指纹认证成功", Toast.LENGTH_LONG).show();
                 startFingerListening();
             }
 
             @Override
             public void onAuthenticationFailed() {
-                Toast.makeText(getContext(),"指纹认证失败", Toast.LENGTH_LONG).show();
+                Toast.makeText(context, "指纹认证失败", Toast.LENGTH_LONG).show();
             }
         };
     }
 
-    private void startFingerListening(){
-
+    private void startFingerListening() {
         fingerprintManager.authenticate(null,
                 cancellationSignal,
                 0,
@@ -91,5 +87,9 @@ public class Unlock extends Fragment{
                 null);
     }
 
-
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        cancellationSignal.cancel();
+    }
 }
